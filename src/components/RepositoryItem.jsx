3ec5dfab8aed554b188bda5f235f85705ca817e7
Francisco Menezes <franciscomenezesbh@gmail.com>
2021-09-26 10:53:47 -0300

    Chmadas API

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -5,7 +5,7 @@ export function RepositoryItem(props){
                 <li>
                     <strong>{props.repository.name ?? 'Defaut'}</strong>
                     <p>{props.repository.description}</p>
-                    <a href={props.repository.link}>Acessar reṕositorio</a>
+                    <a href={props.repository.html_url}>Acessar reṕositorio</a>
                 </li>
     )
 }
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,10 @@
 import { RepositoryItem } from "./RepositoryItem";
 
+import '../styles/repositories.scss'
+import { useState, useEffect } from "react";
+
+//https://api.github.com/users/frantecbh/repos
+
 const repository ={
     name: 'unform',
     description: 'Forms in React',
@@ -7,21 +12,28 @@ const repository ={
 }
 
 export function RepositoryList(){
+
+    const [repositories, setRepostories] = useState([])
+
+    useEffect(()=>{
+
+            fetch('https://api.github.com/users/frantecbh/repos')
+                .then(response => response.json())
+                .then(data => setRepostories(data))        
+
+    },[])
+
     return(
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
             <ul>
-                <RepositoryItem 
-                repository={repository}
-                />
-                <RepositoryItem 
-                repository={repository}
-                />
-                <RepositoryItem 
-                repository={repository}
-                /><RepositoryItem 
-                repository={repository}
-                />
+                {
+                    repositories.map(repository => {
+                        return <RepositoryItem key={repository.name} repository={repository}/>
+                    })
+                }
+                
+               
             </ul>
         </section>
     )
